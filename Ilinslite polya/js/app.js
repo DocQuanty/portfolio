b266952365ed554701743c9c00deb3e881c6644e
Костянтин <71712209+DocQuanty@users.noreply.github.com>
2021-03-27 13:33:01 +0200

    add Ilinslite
    add

---
+++ Ilinslite polya/js/app.js
@@ -0,0 +1,14 @@
+/* Filter om mobile*/
+const sideBarToggleBtn = document.querySelector('.menu-icon-wrapper')
+const menuIcon = document.querySelector('.menu-icon')
+const sideBar = document.querySelector('.sidebar')
+
+
+//Клик по кнопке для показа фильтра и изменения иконки
+
+sideBarToggleBtn.onclick = function () {
+    menuIcon.classList.toggle('menu-icon-active');
+    sideBar.classList.toggle('sidebar--mobile-active');
+
+};
+
